ad3f4a85086491a395f059ccc97af8becc0fa2a9
Mrugelis <martinsrugelis@gmail.com>
2015-10-19 12:20:07 -0400

    First commit

---
+++ GAME 101 Text-RPG/GAME 101 Text-RPG/Enemy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_101_Text_RPG
+{
+    class Enemy
+    {
+
+        string enemyName;
+        int weaponCount;
+        bool isAlive;
+        int currentState;
+
+
+        public Enemy()
+        {
+            enemyName = "enemy";
+            weaponCount = 5;
+            isAlive = true;
+            currentState = 2;
+        }
+
+        public Enemy(string name, int weapon, bool alive, int state)
+        {
+            Name = name;
+            Weapon = weapon;
+            Alive = alive;
+            State = state;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return enemyName;
+            }
+            set{
+                enemyName = value; 
+            }
+        }
+
+        public int Weapon
+        {
+            get
+            {
+                return weaponCount;
+            }
+
+            set
+            {
+                weaponCount = value;
+            }
+        }
+
+        public int State
+        {
+            get
+            {
+                return currentState;
+            }
+
+            set
+            {
+                currentState = value;
+            }
+        }
+
+        public bool Alive
+        {
+       
+            get
+            {
+                return isAlive;
+            }
+
+            set
+            {
+                isAlive = value;
+            }
+      
+        }
+
+        public void enemyAttack()
+        {
+            Console.WriteLine("");
+        }
+
+        public void enemyPatrol()
+        {
+            Console.WriteLine("You hear lumbering, pounding footsteps in the fog.");
+        }
+
+        public void enemyAlert()
+        {
+            Console.WriteLine("Seems that we have a their lurking about!");
+        }
+
+        public void enemyDeath()
+        {
+            Console.WriteLine("You win!");
+        }
+
+    }
+}
---
+++ GAME 101 Text-RPG/GAME 101 Text-RPG/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME_101_Text_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy firstEnemy = new Enemy();
+            Enemy secondEnemy = new Enemy("Bob", 3, true, 10);
+            int gameState = 1;
+            string myPlayerName;
+            string myDecision;
+      
+           Console.WriteLine("Enter Player Name");
+           myPlayerName = Console.ReadLine();
+           Console.WriteLine("Welcome {0}, you wake in a dense fog. Only to see a dark silhouette fading into the distance.", myPlayerName);
+           Console.WriteLine("Do you follow the mysterious figure? y / n");
+
+            do
+           {
+               myDecision = Console.ReadLine();
+
+               if (myDecision == "y")
+               {
+                   Console.WriteLine("As you head towards the figure");
+                   gameState = 2;
+               }
+               else if (myDecision == "n")
+               {
+                   Console.WriteLine("You wander in the opposite direction, hoping to find an indication of where you are.");
+                   firstEnemy.enemyPatrol();
+                   gameState = 0;
+               }
+           } while (gameState == 1);
+
+           if (gameState == 0)
+           {
+               Console.WriteLine("impending doom");
+           }
+
+
+
+           if (gameState == 2)
+           {
+               Console.WriteLine("story continues");
+
+           }
+        }
+    }
+}
+
