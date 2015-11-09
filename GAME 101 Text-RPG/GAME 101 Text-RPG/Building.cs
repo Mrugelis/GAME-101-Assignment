bae046338bfbd215cd48aa39b5908b15070eaddf
Mrugelis <martinsrugelis@gmail.com>
2015-11-09 14:23:46 -0500

    room stuff

--- GAME 101 Text-RPG/GAME 101 Text-RPG/Building.cs
+++ GAME 101 Text-RPG/GAME 101 Text-RPG/Building.cs
@@ -12,19 +12,41 @@ namespace GAME_101_Text_RPG
 
         public Building()
         {
-            Rooms _1_1;
-            Rooms _1_2;
-            Rooms _1_3;
-            Dictionary<int, Rooms> roomChoices = new Dictionary<int, Rooms>;    
+            fillBuilding();
+        }
+
+        public void fillBuilding()
+        {
+             //fills Building with rooms
+            Room R1_1;
+            Room R1_2;
+            Room R1_3;
+            Room R1_4;
+            Room R1_5;
+
+            Room R2_1;
+            Room R2_2;
+            Room R2_3;
+            Room R2_4;
+            Room R2_5;
+
+            Room R3_1;
+            Room R3_2;
+            Room R3_3;
+            Room R3_4;
+            Room R3_5;
         }
     }
+}
 
-    struct Rooms
-    {
-        public string choice1;
+
+
+struct Room
+{
+       public Dictionary<int,string> choice1;
         public string choice2;
         public string choice3;
         public string choice4;
         public string choice5;
-    };
-}
+};
+
